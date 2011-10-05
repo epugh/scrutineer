0ae76c054cbbd1eb5e58a64f9fd3ec9d62004788
psmith <psmith@aconex.com>
2011-10-05 16:21:43 +1100

    Brave assumption in test that '/tmp' is the temporary directory on all platforms, this should work on Linux and OSX at least, althouh I need to check W32.

--- src/test/java/com/aconex/scrutineer/elasticsearch/ElasticSearchIdAndVersionStreamTest.java
+++ src/test/java/com/aconex/scrutineer/elasticsearch/ElasticSearchIdAndVersionStreamTest.java
@@ -1,16 +1,5 @@
 package com.aconex.scrutineer.elasticsearch;
 
-import com.aconex.scrutineer.IdAndVersion;
-import org.apache.commons.lang.SystemUtils;
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-
-import java.io.File;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.Iterator;
-
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.doReturn;
@@ -18,6 +7,18 @@ import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.MockitoAnnotations.initMocks;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Iterator;
+
+import com.aconex.scrutineer.IdAndVersion;
+import org.apache.commons.lang.SystemUtils;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
 
 @SuppressWarnings("unsafe")
 public class ElasticSearchIdAndVersionStreamTest {
@@ -45,8 +46,8 @@ public class ElasticSearchIdAndVersionStreamTest {
         initMocks(this);
     }
 
-    @Test public void shouldDownloadAndSortOnSetup() {
-        ElasticSearchIdAndVersionStream elasticSearchIdAndVersionStream = spy(new ElasticSearchIdAndVersionStream(elasticSearchDownloader, elasticSearchSorter, iteratorFactory, "/tmp"));
+    @Test public void shouldDownloadAndSortOnSetup() throws IOException {
+        ElasticSearchIdAndVersionStream elasticSearchIdAndVersionStream = spy(new ElasticSearchIdAndVersionStream(elasticSearchDownloader, elasticSearchSorter, iteratorFactory, SystemUtils.getJavaIoTmpDir().getAbsolutePath()));
         doReturn(sortedOutputStream).when(elasticSearchIdAndVersionStream).createSortedOutputStream();
         doReturn(unSortedOutputStream).when(elasticSearchIdAndVersionStream).createUnsortedOutputStream();
         doReturn(unSortedInputStream).when(elasticSearchIdAndVersionStream).createUnSortedInputStream();
@@ -55,8 +56,8 @@ public class ElasticSearchIdAndVersionStreamTest {
         verify(elasticSearchSorter).sort(unSortedInputStream,sortedOutputStream);
     }
 
-    @Test public void shouldReturnAFileIterator() {
-        ElasticSearchIdAndVersionStream elasticSearchIdAndVersionStream = new ElasticSearchIdAndVersionStream(elasticSearchDownloader, elasticSearchSorter, iteratorFactory, "/tmp");
+    @Test public void shouldReturnAFileIterator() throws IOException {
+        ElasticSearchIdAndVersionStream elasticSearchIdAndVersionStream = new ElasticSearchIdAndVersionStream(elasticSearchDownloader, elasticSearchSorter, iteratorFactory, SystemUtils.getJavaIoTmpDir().getAbsolutePath());
         when(iteratorFactory.forFile(new File(SystemUtils.getJavaIoTmpDir(),SORTED_FILENAME))).thenReturn(iterator);
         assertThat(elasticSearchIdAndVersionStream.iterator(), is(iterator));
     }
