a4159ff7d37ea7a13ab5eafce0de8c722e87601e
Leon <leonm@aconex.com>
2011-10-06 13:35:17 +1100

    KAT-466 Added a file iterator and factory.

--- src/main/java/com/aconex/scrutineer/elasticsearch/IteratorFactory.java
+++ src/main/java/com/aconex/scrutineer/elasticsearch/IteratorFactory.java
@@ -2,11 +2,21 @@ package com.aconex.scrutineer.elasticsearch;
 
 import com.aconex.scrutineer.IdAndVersion;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.util.Iterator;
 
 public class IteratorFactory {
     public Iterator<IdAndVersion> forFile(File file) {
-        return null;
+        try {
+            return new IdAndVersionInputStreamIterator(new IdAndVersionDataReader(
+                    new ObjectInputStream(new BufferedInputStream(new FileInputStream(file)))
+            ));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/com/aconex/scrutineer/elasticsearch/IdAndVersionInputStreamIteratorTest.java
@@ -0,0 +1,64 @@
+package com.aconex.scrutineer.elasticsearch;
+
+import com.aconex.scrutineer.IdAndVersion;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.io.IOException;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.when;
+
+public class IdAndVersionInputStreamIteratorTest {
+
+    private static final long ID = 12L;
+    private static final long VERSION = 77L;
+    @Mock
+    private IdAndVersionDataReader idAndVersionDataReader;
+
+    @Before
+    public void setup() throws IOException {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test(expected = UnsupportedOperationException.class)
+    public void shouldThrowUnsupportedExceptionOnRemove() {
+        IdAndVersionInputStreamIterator idAndVersionInputStreamIterator = new IdAndVersionInputStreamIterator(idAndVersionDataReader);
+        idAndVersionInputStreamIterator.remove();
+    }
+
+    @Test
+    public void hasNextShouldReturnFalseForAnEmptyStream() throws IOException {
+        when(idAndVersionDataReader.readNext()).thenReturn(null);
+        IdAndVersionInputStreamIterator idAndVersionInputStreamIterator = new IdAndVersionInputStreamIterator(idAndVersionDataReader);
+        assertThat(idAndVersionInputStreamIterator.hasNext(), is(false));
+    }
+
+    @Test
+    public void hasNextShouldReturnTrueForNonEmptyStream() throws IOException {
+        when(idAndVersionDataReader.readNext()).thenReturn(new IdAndVersion(ID, VERSION));
+        IdAndVersionInputStreamIterator idAndVersionInputStreamIterator = new IdAndVersionInputStreamIterator(idAndVersionDataReader);
+        assertThat(idAndVersionInputStreamIterator.hasNext(), is(true));
+    }
+
+    @Test
+    public void shouldGetTheNextItem() throws IOException {
+        IdAndVersion idAndVersion = new IdAndVersion(ID, VERSION);
+        when(idAndVersionDataReader.readNext()).thenReturn(idAndVersion);
+        IdAndVersionInputStreamIterator idAndVersionInputStreamIterator = new IdAndVersionInputStreamIterator(idAndVersionDataReader);
+        assertThat(idAndVersionInputStreamIterator.next(), is(idAndVersion));
+    }
+
+    @Test
+    public void hasNextShouldReturnFalseAtEndOfTheStream() throws IOException {
+        IdAndVersion idAndVersion = new IdAndVersion(ID, VERSION);
+        when(idAndVersionDataReader.readNext()).thenReturn(idAndVersion).thenReturn(null);
+        IdAndVersionInputStreamIterator idAndVersionInputStreamIterator = new IdAndVersionInputStreamIterator(idAndVersionDataReader);
+        assertThat(idAndVersionInputStreamIterator.next(), is(idAndVersion));
+        assertThat(idAndVersionInputStreamIterator.hasNext(), is(false));
+    }
+
+}
