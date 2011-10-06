b8c920d36e05f1f86bcdc3da457f0517a6f1ecb0
Leon <leonm@aconex.com>
2011-10-07 09:13:50 +1100

    KAT-466 Introduced a listener class to the verifier Added a test to ensure that equal streams do not fire any events on the listener.

--- src/main/java/com/aconex/scrutineer/IdAndVersionStreamVerifier.java
+++ src/main/java/com/aconex/scrutineer/IdAndVersionStreamVerifier.java
@@ -2,7 +2,7 @@ package com.aconex.scrutineer;
 
 public class IdAndVersionStreamVerifier {
 
-    public void verify(IdAndVersionStream primaryStream, IdAndVersionStream secondayStream) {
+    public void verify(IdAndVersionStream primaryStream, IdAndVersionStream secondayStream, IdAndVersionStreamVerifierListener idAndVersionStreamVerifierListener) {
         primaryStream.open();
         secondayStream.open();
         try {
--- src/test/java/com/aconex/scrutineer/IdAndVersionStreamVerifierTest.java
+++ src/test/java/com/aconex/scrutineer/IdAndVersionStreamVerifierTest.java
@@ -1,19 +1,33 @@
 package com.aconex.scrutineer;
 
+import com.aconex.scrutineer.javautil.JavaIteratorIdAndVersionStream;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 
+import java.util.List;
+
+import static com.google.common.collect.Lists.newArrayList;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 public class IdAndVersionStreamVerifierTest {
 
+    private static final List<IdAndVersion> LIST = newArrayList(
+            item("1", 1),
+            item("3", 3),
+            item("2", 2));
+
     @Mock
     private IdAndVersionStream primaryStream;
+
     @Mock
     private IdAndVersionStream secondayStream;
 
+    @Mock
+    private IdAndVersionStreamVerifierListener idAndVersionStreamVerifierListener;
+    
     @Before
     public void setup() {
         initMocks(this);
@@ -22,7 +36,7 @@ public class IdAndVersionStreamVerifierTest {
     @Test
     public void shouldOpenBothStreams() {
         IdAndVersionStreamVerifier idAndVersionStreamVerifier = new IdAndVersionStreamVerifier();
-        idAndVersionStreamVerifier.verify(primaryStream, secondayStream);
+        idAndVersionStreamVerifier.verify(primaryStream, secondayStream, idAndVersionStreamVerifierListener);
         verify(primaryStream).open();
         verify(secondayStream).open();
     }
@@ -30,8 +44,22 @@ public class IdAndVersionStreamVerifierTest {
     @Test
     public void shouldCloseBothStreams() {
         IdAndVersionStreamVerifier idAndVersionStreamVerifier = new IdAndVersionStreamVerifier();
-        idAndVersionStreamVerifier.verify(primaryStream, secondayStream);
+        idAndVersionStreamVerifier.verify(primaryStream, secondayStream, idAndVersionStreamVerifierListener);
         verify(primaryStream).close();
         verify(secondayStream).close();
+    }
+
+    @Test
+    public void shouldNotReportErrorsIfStreamsAreEqual() {
+        IdAndVersionStreamVerifier idAndVersionStreamVerifier = new IdAndVersionStreamVerifier();
+        idAndVersionStreamVerifier.verify(
+                new JavaIteratorIdAndVersionStream(LIST.iterator()),
+                new JavaIteratorIdAndVersionStream(LIST.iterator()),
+                idAndVersionStreamVerifierListener);
+        verifyZeroInteractions(idAndVersionStreamVerifierListener);
+    }
+
+    static IdAndVersion item(String id, long version) {
+        return new IdAndVersion(id, version);
     }
 }
