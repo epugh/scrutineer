b98259709d1a1818354849c0a2000c06b8087d62
psmith <psmith@aconex.com>
2011-10-21 16:43:46 +1100

    Fix checkstyle issue by trying to deal with this shitty JDBC resource closing in a clean manner, not feeling great, but ...

--- src/main/java/com/aconex/scrutineer/jdbc/JdbcIdAndVersionStream.java
+++ src/main/java/com/aconex/scrutineer/jdbc/JdbcIdAndVersionStream.java
@@ -1,16 +1,19 @@
 package com.aconex.scrutineer.jdbc;
 
-import com.aconex.scrutineer.IdAndVersion;
-import com.aconex.scrutineer.IdAndVersionStream;
-import com.aconex.scrutineer.LogUtils;
-import org.apache.log4j.Logger;
-
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
 import java.util.Iterator;
+
+import com.aconex.scrutineer.IdAndVersion;
+import com.aconex.scrutineer.IdAndVersionStream;
+import com.aconex.scrutineer.LogUtils;
+import com.google.common.base.Predicates;
+import com.google.common.collect.Iterables;
+import org.apache.log4j.Logger;
 
 public class JdbcIdAndVersionStream implements IdAndVersionStream {
 
@@ -48,38 +51,52 @@ public class JdbcIdAndVersionStream implements IdAndVersionStream {
 
     @Override
     public void close() {
+        throwExceptionIfAnyCloseFails(closeResultSet(), closeStatement(), closeConnection());
+    }
 
+    private void throwExceptionIfAnyCloseFails(SQLException... sqlExceptions) {
+        if (!gIterables.all(Arrays.asList(sqlExceptions), Predicates.<Object>isNull())) {
+            throw new RuntimeException("At least one error occured during close, see logs for more details, there may be multiple");
+        }
+    }
+
+    private SQLException closeConnection() {
         SQLException sqlException = null;
-        
-        if (resultSet != null) {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                sqlException = e;
-                LogUtils.error(LOG, "Cannot close resultset",e);
-            }
-        }
-        if (statement != null) {
-            try {
-                statement.close();
-            } catch (SQLException e) {
-                sqlException = e;
-                LogUtils.error(LOG, "Cannot close statement",e);
-            }
-        }
-
         if (connection != null) {
             try {
                 connection.close();
             } catch (SQLException e) {
                 sqlException = e;
-                LogUtils.error(LOG, "Cannot close connection",e);
+                LogUtils.error(LOG, "Cannot close connection", e);
             }
         }
+        return sqlException;
+    }
 
-        if (sqlException != null) {
-            throw new RuntimeException(sqlException);
+    private SQLException closeStatement() {
+        SQLException sqlException = null;
+        if (statement != null) {
+            try {
+                statement.close();
+            } catch (SQLException e) {
+                sqlException = e;
+                LogUtils.error(LOG, "Cannot close statement", e);
+            }
         }
+        return sqlException;
+    }
+
+    private SQLException closeResultSet() {
+        SQLException sqlException = null;
+        if (resultSet != null) {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                sqlException = e;
+                LogUtils.error(LOG, "Cannot close resultset", e);
+            }
+        }
+        return sqlException;
     }
 
 }
