87c4f033324b2e23542ca34afc697803bf1240a9
Paul Smith <psmith@aconex.com>
2013-04-05 20:57:06 +1100

    Introduce a Function that formats the version value when being output, with the default merely passing it through natively.
    This will allow introduction of formatting options for cases where the version value is a timestamp, so we can output the values later on in ISO8601 format.

--- src/main/java/com/aconex/scrutineer/PrintStreamOutputVersionStreamVerifierListener.java
+++ src/main/java/com/aconex/scrutineer/PrintStreamOutputVersionStreamVerifierListener.java
@@ -1,28 +1,42 @@
 package com.aconex.scrutineer;
 
+import com.google.common.base.Function;
+
 import java.io.PrintStream;
 
 public class PrintStreamOutputVersionStreamVerifierListener implements IdAndVersionStreamVerifierListener {
 
-
     private final PrintStream printStream;
+    private final Function<Long, Object> versionFormatter;
 
     public PrintStreamOutputVersionStreamVerifierListener(PrintStream printStream) {
+        this(printStream, new DefaultVersionFormatter());
+    }
+
+    public PrintStreamOutputVersionStreamVerifierListener(PrintStream printStream, Function<Long, Object> versionFormatter) {
         this.printStream = printStream;
+        this.versionFormatter = versionFormatter;
     }
 
     @Override
     public void onMissingInSecondaryStream(IdAndVersion idAndVersion) {
-        printStream.println(String.format("NOTINSECONDARY\t%s\t%d",idAndVersion.getId(), idAndVersion.getVersion()));
+        printStream.println(String.format("NOTINSECONDARY\t%s\t%s", idAndVersion.getId(), versionFormatter.apply(idAndVersion.getVersion())));
     }
 
     @Override
     public void onMissingInPrimaryStream(IdAndVersion idAndVersion) {
-        printStream.println(String.format("NOTINPRIMARY\t%s\t%d",idAndVersion.getId(), idAndVersion.getVersion()));
+        printStream.println(String.format("NOTINPRIMARY\t%s\t%s", idAndVersion.getId(), versionFormatter.apply(idAndVersion.getVersion())));
     }
 
     @Override
     public void onVersionMisMatch(IdAndVersion primaryItem, IdAndVersion secondaryItem) {
-        printStream.println(String.format("MISMATCH\t%s\t%d\tsecondaryVersion=%d",primaryItem.getId(), primaryItem.getVersion(), secondaryItem.getVersion()));
+        printStream.println(String.format("MISMATCH\t%s\t%s\tsecondaryVersion=%s", primaryItem.getId(), versionFormatter.apply(primaryItem.getVersion()), versionFormatter.apply(secondaryItem.getVersion())));
+    }
+
+    private static class DefaultVersionFormatter implements Function<Long, Object> {
+        @Override
+        public Object apply(Long aLong) {
+            return aLong;
+        }
     }
 }
--- src/test/java/com/aconex/scrutineer/PrintStreamOutputVersionStreamVerifierListenerTest.java
+++ src/test/java/com/aconex/scrutineer/PrintStreamOutputVersionStreamVerifierListenerTest.java
@@ -1,5 +1,6 @@
 package com.aconex.scrutineer;
 
+import com.google.common.base.Function;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
@@ -7,8 +8,8 @@ import org.mockito.MockitoAnnotations;
 
 import java.io.PrintStream;
 
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
+import static org.mockito.Matchers.anyLong;
+import static org.mockito.Mockito.*;
 
 public class PrintStreamOutputVersionStreamVerifierListenerTest {
 
@@ -17,8 +18,11 @@ public class PrintStreamOutputVersionStreamVerifierListenerTest {
     @Mock
     private PrintStream printStream;
 
+    @Mock
+    private Function<Long, Object> versionFormatter;
+
     @Before
-    public void setup(){
+    public void setup() {
         MockitoAnnotations.initMocks(this);
     }
 
@@ -38,12 +42,40 @@ public class PrintStreamOutputVersionStreamVerifierListenerTest {
         verifyNoMoreInteractions(printStream);
     }
 
-
     @Test
     public void testOnVersionMisMatch() throws Exception {
         PrintStreamOutputVersionStreamVerifierListener streamVerifierListener = new PrintStreamOutputVersionStreamVerifierListener(printStream);
-        streamVerifierListener.onVersionMisMatch(idAndVersion, new StringIdAndVersion("1",9));
+        streamVerifierListener.onVersionMisMatch(idAndVersion, new StringIdAndVersion("1", 9));
         verify(printStream).println("MISMATCH\t1\t10\tsecondaryVersion=9");
         verifyNoMoreInteractions(printStream);
     }
+
+    @Test
+    public void testVersionIsFormattedForMismatch() {
+        when(versionFormatter.apply(anyLong())).thenReturn("9 Bottles of Beer on the Wall");
+
+        PrintStreamOutputVersionStreamVerifierListener streamVerifierListener = new PrintStreamOutputVersionStreamVerifierListener(printStream, versionFormatter);
+        streamVerifierListener.onVersionMisMatch(idAndVersion, new StringIdAndVersion("1", 9));
+        verify(printStream).println("MISMATCH\t1\t9 Bottles of Beer on the Wall\tsecondaryVersion=9 Bottles of Beer on the Wall");
+    }
+
+    @Test
+    public void testVersionIsFormattedForNotInPrimary() {
+        when(versionFormatter.apply(anyLong())).thenReturn("10 Bottles of Beer on the Wall");
+
+        PrintStreamOutputVersionStreamVerifierListener streamVerifierListener = new PrintStreamOutputVersionStreamVerifierListener(printStream, versionFormatter);
+        streamVerifierListener.onMissingInPrimaryStream(idAndVersion);
+        verify(printStream).println("NOTINPRIMARY\t1\t10 Bottles of Beer on the Wall");
+    }
+
+    @Test
+    public void testVersionIsFormattedForNotInSecondary() {
+        when(versionFormatter.apply(anyLong())).thenReturn("10 Bottles of Beer on the Wall");
+
+        PrintStreamOutputVersionStreamVerifierListener streamVerifierListener = new PrintStreamOutputVersionStreamVerifierListener(printStream, versionFormatter);
+        streamVerifierListener.onMissingInSecondaryStream(idAndVersion);
+        verify(printStream).println("NOTINSECONDARY\t1\t10 Bottles of Beer on the Wall");
+    }
+
+
 }
