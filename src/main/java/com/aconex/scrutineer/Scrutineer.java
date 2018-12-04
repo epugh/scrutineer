80420f1156cac4c48ee5d540ba7c26550da9c6dd
psmith <psmith@aconex.com>
2018-12-05 09:51:00 +1100

    Fixes for core classes since ES upgrade (tests updated in subsequent commit)

--- src/main/java/com/aconex/scrutineer/Scrutineer.java
+++ src/main/java/com/aconex/scrutineer/Scrutineer.java
@@ -21,7 +21,7 @@ import com.google.common.base.Function;
 import org.apache.commons.lang3.SystemUtils;
 import org.apache.log4j.xml.DOMConfigurator;
 import org.elasticsearch.client.Client;
-import org.elasticsearch.node.Node;
+import org.elasticsearch.transport.client.PreBuiltTransportClient;
 import org.slf4j.Logger;
 
 public class Scrutineer {
@@ -70,9 +70,6 @@ public class Scrutineer {
     void closeElasticSearchConnections() {
         if (client != null) {
             client.close();
-        }
-        if (node != null) {
-            node.close();
         }
     }
 
@@ -127,8 +124,7 @@ public class Scrutineer {
     }
 
     ElasticSearchIdAndVersionStream createElasticSearchIdAndVersionStream(ScrutineerCommandLineOptions options) {
-        this.node = new NodeFactory().createNode(options);
-        this.client = node.client();
+        this.client = new PreBuiltTransportClient(new NodeFactory().createSettings(options));
         return new ElasticSearchIdAndVersionStream(new ElasticSearchDownloader(client, options.indexName, options.query, idAndVersionFactory), new ElasticSearchSorter(createSorter()), new IteratorFactory(idAndVersionFactory), SystemUtils.getJavaIoTmpDir().getAbsolutePath());
     }
 
@@ -157,7 +153,6 @@ public class Scrutineer {
     private static final int DEFAULT_SORT_MEM = 256 * 1024 * 1024;
     private final ScrutineerCommandLineOptions options;
     private IdAndVersionFactory idAndVersionFactory;
-    private Node node;
     private Client client;
     private Connection connection;
 
