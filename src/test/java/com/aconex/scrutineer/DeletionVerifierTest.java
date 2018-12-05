c9ea9b20b18224239c4f80ac3fb441485a0c6b0d
psmith <psmith@aconex.com>
2018-12-05 16:50:46 +1100

    Mockito 2 upgrade - remove deprecation warnings from test code migrating to new Mockito namespace.

--- src/test/java/com/aconex/scrutineer/DeletionVerifierTest.java
+++ src/test/java/com/aconex/scrutineer/DeletionVerifierTest.java
@@ -1,17 +1,17 @@
 package com.aconex.scrutineer;
 
-import org.junit.Before;
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 import java.util.Arrays;
 import java.util.List;
 
-import static org.mockito.Matchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 
 public class DeletionVerifierTest {
 
