ad48bdb983eea64272c17d35f631b336aedf0716
psmith <psmith@aconex.com>
2018-12-05 16:50:46 +1100

    Mockito 2 upgrade - remove deprecation warnings from test code migrating to new Mockito namespace.

--- src/test/java/com/aconex/scrutineer/elasticsearch/ElasticSearchDownloaderTest.java
+++ src/test/java/com/aconex/scrutineer/elasticsearch/ElasticSearchDownloaderTest.java
@@ -5,7 +5,7 @@ import static com.aconex.scrutineer.elasticsearch.ElasticSearchDownloader.SCROLL
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
-import static org.mockito.Matchers.any;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
