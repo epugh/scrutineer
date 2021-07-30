032f367429e301aecac00ce8e11a81bc7b3fe864
Rock YU <rockyu@aconex.com>
2021-07-30 15:15:55 +1000

    modularize scrutineer: extract jdbc module which implements IdAndVersionStream which iterates over a Jdbc query result

--- jdbc/src/test/java/com/aconex/scrutineer/jdbc/JdbcIdAndVersionStreamTest.java
+++ jdbc/src/test/java/com/aconex/scrutineer/jdbc/JdbcIdAndVersionStreamTest.java
@@ -1,13 +1,6 @@
 package com.aconex.scrutineer.jdbc;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.fail;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.never;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -15,13 +8,15 @@ import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+import com.aconex.scrutineer.IdAndVersionFactory;
+import com.aconex.scrutineer.StringIdAndVersion;
+import org.hamcrest.core.Is;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
-
-import com.aconex.scrutineer.IdAndVersionFactory;
-import com.aconex.scrutineer.StringIdAndVersion;
 
 public class JdbcIdAndVersionStreamTest {
 
@@ -46,62 +41,62 @@ public class JdbcIdAndVersionStreamTest {
 
     @Test
     public void shouldCloseDBResourcesEvenIfNoIteration() throws SQLException {
-        when(connection.createStatement()).thenReturn(statement);
-        when(statement.executeQuery(SQL)).thenReturn(resultSet);
-        when(resultSet.getMetaData()).thenReturn(metaData);
+        Mockito.when(connection.createStatement()).thenReturn(statement);
+        Mockito.when(statement.executeQuery(SQL)).thenReturn(resultSet);
+        Mockito.when(resultSet.getMetaData()).thenReturn(metaData);
 
         JdbcIdAndVersionStream jdbcIdAndVersionStream = new JdbcIdAndVersionStream(connection, SQL, idAndVersionFactory);
         jdbcIdAndVersionStream.open();
         jdbcIdAndVersionStream.close();
 
-        verify(statement).close();
-        verify(resultSet).close();
-        verify(connection, never()).close();
+        Mockito.verify(statement).close();
+        Mockito.verify(resultSet).close();
+        Mockito.verify(connection, Mockito.never()).close();
     }
 
     @Test
     public void closeShouldDoNothingIfNotOpen() throws SQLException {
         JdbcIdAndVersionStream jdbcIdAndVersionStream = new JdbcIdAndVersionStream(connection, SQL, idAndVersionFactory);
         jdbcIdAndVersionStream.close();
-        verifyNoMoreInteractions(connection);
+        Mockito.verifyNoMoreInteractions(connection);
     }
 
     @Test
     public void shouldExecuteSQLQuery() throws SQLException {
-        when(connection.createStatement()).thenReturn(statement);
+        Mockito.when(connection.createStatement()).thenReturn(statement);
         //TODO: Handle scrolling properly
-        when(statement.executeQuery(SQL)).thenReturn(resultSet);
-        when(resultSet.getMetaData()).thenReturn(metaData);
+        Mockito.when(statement.executeQuery(SQL)).thenReturn(resultSet);
+        Mockito.when(resultSet.getMetaData()).thenReturn(metaData);
         JdbcIdAndVersionStream jdbcIdAndVersionStream = new JdbcIdAndVersionStream(connection, SQL, idAndVersionFactory);
         jdbcIdAndVersionStream.open();
         IdAndVersionResultSetIterator iterator = (IdAndVersionResultSetIterator) jdbcIdAndVersionStream.iterator();
-        assertThat(iterator.getResultSet(), is(resultSet));
-        verify(statement).executeQuery(SQL);
+        assertThat(iterator.getResultSet(), Is.is(resultSet));
+        Mockito.verify(statement).executeQuery(SQL);
     }
 
     @Test
     public void shouldTearDownStatement() throws SQLException {
-        when(connection.createStatement()).thenReturn(statement);
-        when(statement.executeQuery(SQL)).thenReturn(resultSet);
-        when(resultSet.getMetaData()).thenReturn(metaData);
+        Mockito.when(connection.createStatement()).thenReturn(statement);
+        Mockito.when(statement.executeQuery(SQL)).thenReturn(resultSet);
+        Mockito.when(resultSet.getMetaData()).thenReturn(metaData);
         JdbcIdAndVersionStream jdbcIdAndVersionStream = new JdbcIdAndVersionStream(connection, SQL, idAndVersionFactory);
         jdbcIdAndVersionStream.open();
         jdbcIdAndVersionStream.iterator();
         jdbcIdAndVersionStream.close();
-        verify(resultSet).close();
-        verify(statement).close();
+        Mockito.verify(resultSet).close();
+        Mockito.verify(statement).close();
     }
 
     @Test(expected = RuntimeException.class)
     public void shouldThrowExceptionIfResultSetFailsToClose() throws SQLException {
         try {
-            when(connection.createStatement()).thenReturn(statement);
-            when(statement.executeQuery(SQL)).thenReturn(resultSet);
+            Mockito.when(connection.createStatement()).thenReturn(statement);
+            Mockito.when(statement.executeQuery(SQL)).thenReturn(resultSet);
         } catch (SQLException e) {
             e.printStackTrace();
-            fail("Unexpected exception");
+            Assert.fail("Unexpected exception");
         }
-        doThrow(new SQLException()).when(resultSet).close();
+        Mockito.doThrow(new SQLException()).when(resultSet).close();
         JdbcIdAndVersionStream jdbcIdAndVersionStream = new JdbcIdAndVersionStream(connection, SQL, idAndVersionFactory);
         jdbcIdAndVersionStream.open();
         jdbcIdAndVersionStream.iterator();
